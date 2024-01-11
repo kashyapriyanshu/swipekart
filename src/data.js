0de46d389008960b872c28742b3c2c6795292237
Kashyapriyanshu <kashyapriyanshu2002@gmail.com>
2024-01-11 20:09:49 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,62 @@
+export const products = [
+  {
+    id: 1,
+    name: "TFG deep bass air",
+    price: 1359,
+    image: "/assets/img/01.webp",
+  },
+  {
+    id: 2,
+    name: "Realme Buds Air 5",
+    price: 1999,
+    image: "/assets/img/02.webp",
+  },
+  {
+    id: 3,
+    name: "One plus bluetooth wireless",
+    price: 1500,
+    image: "/assets/img/03.webp",
+  },
+  {
+    id: 4,
+    name: "HP wired mouse",
+    price: 800,
+    image: "/assets/img/11.webp",
+  },
+  {
+    id: 5,
+    name: "Asus Tuf gaming",
+    price: 68999,
+    image: "/assets/img/04.webp",
+  },
+  {
+    id: 6,
+    name: "San Disk Pen drive",
+    price: 1500,
+    image: "/assets/img/13.webp",
+  },
+  {
+    id: 7,
+    name: "Fredd - Black and White",
+    price: 1801.1,
+    image: "/assets/img/7.png",
+  },
+  {
+    id: 8,
+    name: "Star Wars - The Last",
+    price: 1199.99,
+    image: "/assets/img/8.png",
+  },
+  {
+    id: 9,
+    name: "Yellow Blouse",
+    price: 2395.16,
+    image: "/assets/img/9.png",
+  },
+  {
+    id: 10,
+    name: "Rick and Morty - Supreme",
+    price: 1243.82,
+    image: "/assets/img/10.png",
+  },
+];
